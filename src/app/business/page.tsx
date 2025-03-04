b32ed081b8bde7204e963d104df1dc1d8e49d2de
Chuanyongniu <lnylszjs@163.com>
2025-03-04 10:14:54 +0800

    Add all components and pages

---
+++ src/app/business/page.tsx
@@ -0,0 +1,1 @@
+import BusinessSection from "@/components/business/BusinessSection";export default function BusinessPage(){return(<div className="pt-20"><BusinessSection/></div>);}
---
+++ src/app/cases/page.tsx
@@ -0,0 +1,1 @@
+import CaseGrid from "@/components/cases/CaseGrid";export default function CasesPage(){return(<div className="pt-20"><CaseGrid/></div>);}
